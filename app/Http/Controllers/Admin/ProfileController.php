19f03c33dd30561efd37b98e44b840e7128f3f76
mi-ogura <m-ogura@cmind-co.jp>
2020-07-13 10:45:19 +0900

    第10部課題

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     //
@@ -15,6 +17,16 @@ class ProfileController extends Controller
 
 public function create(Request $request)
 {
+    // Varidationを行う
+    $this->validate($request, Profile::$rules);
+
+    $profile = new Profile;
+    $form = $request->all();
+
+    //データベースに保存する
+    $profile->fill($form);
+    $profile->save();
+
     return redirect('admin/profile/create');
 }
 
--- database/migrations/2020_07_09_061017_create_news_table.php
+++ database/migrations/2020_07_09_061017_create_news_table.php
@@ -11,17 +11,21 @@ class CreateNewsTable extends Migration
      *
      * @return void
      */
+    //5つのカラムを持つnewsテーブル
     public function up()
     {
         Schema::create('news', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('title'); // ニュースのタイトルを保存するカラム
+            $table->string('body');  // ニュースの本文を保存するカラム
+            $table->string('image_path')->nullable();  // 画像のパスを保存するカラム
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
-     *
+     *　関数の取り消し
      * @return void
      */
     public function down()
