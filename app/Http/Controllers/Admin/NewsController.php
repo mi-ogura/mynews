9da448e2c083855c600975924fff654180424614
mi-ogura <m-ogura@cmind-co.jp>
2020-07-14 09:22:39 +0900

    第12部

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\News;
+use App\History;
+use Carbon\Carbon;
 
 class NewsController extends Controller
 {
@@ -66,29 +68,34 @@ class NewsController extends Controller
         return view('admin.news.edit', ['news_form' => $news]);
     }
   
-  
-    public function update(Request $request)
+      public function update(Request $request)
     {
-        // Validationをかける
         $this->validate($request, News::$rules);
-        // News Modelからデータを取得する
-        $news = News::find($request->id);
-        // 送信されてきたフォームデータを格納する
+        $news = News::find($request->input('id'));
         $news_form = $request->all();
-        if (isset($news_form['image'])) {
+        if ($request->input('remove')) {
+            $news_form['image_path'] = null;
+        } elseif ($request->file('image')) {
             $path = $request->file('image')->store('public/image');
-            $news->image_path = basename($path);
-            unset($news_form['image']);
-          } elseif (0 == strcmp($request->remove, 'true')) {
-            $news->image_path = null;
-          }
+            $news_form['image_path'] = basename($path);
+        } else {
+            $news_form['image_path'] = $news->image_path;
+        }
+
         unset($news_form['_token']);
+        unset($news_form['image']);
         unset($news_form['remove']);
-        // 該当するデータを上書きして保存する
         $news->fill($news_form)->save();
 
-        return redirect('admin/news');
-  }
+        // 以下を追記
+        $history = new History;
+        $history->news_id = $news->id;
+        $history->edited_at = Carbon::now();
+        $history->save();
+
+        return redirect('admin/news/');
+    }
+    
 
   public function delete(Request $request)
   {
